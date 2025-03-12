11049530ace21a528fcdb7fcf9f2b54bdcc4ab8e
ThomasADarnell <tomadarnell@gmail.com>
2025-03-12 00:55:04 -0700

    Added extra credit

--- hw2/Assets/Scripts/Coin.cs
+++ hw2/Assets/Scripts/Coin.cs
@@ -6,22 +6,18 @@ public class Coin : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    // Called when an object collides with the coin
-    private void OnTriggerEnter(Collider otherObject)
+    private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log("An object entered the coin's trigger area");
-        FindFirstObjectByType<GameManager>().CoinCollected();
-
-        //Destroy the coin when the player collides with it (collect it)
-        Destroy (this.gameObject);
+        FindFirstObjectByType<GameManager>().GetComponent<GameManager>().CoinCollected();
+        Destroy(this.gameObject);
     }
 }
--- hw2/Assets/Scripts/GameManager.cs
+++ hw2/Assets/Scripts/GameManager.cs
@@ -1,32 +1,34 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public int collectedCoins;
-    public int totalNumberOfCoins;
+    public int CollectedCoins;
+    public int TotalNumberOfCoins;
+    public TextMeshProUGUI CoinCounter;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.totalNumberOfCoins = FindObjectsByType<Coin>(FindObjectsSortMode.None).Length;
-        Debug.Log("Coins To Find: " + totalNumberOfCoins);
-        
+        this.TotalNumberOfCoins = FindObjectsByType<Coin>(FindObjectsSortMode.None).Length;
+        Debug.Log("Total number of coins to collect: " + this.TotalNumberOfCoins);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(this.collectedCoins == this.totalNumberOfCoins)
+        if (this.CollectedCoins >= this.TotalNumberOfCoins)
         {
             Debug.Log("You won!");
-            Debug.Break();
+            SceneManager.LoadScene("MenuScene");
         }
     }
 
     public void CoinCollected()
     {
-        this.collectedCoins++;
-        Debug.Log("Coins Collected: " + this.collectedCoins + "/" + this.totalNumberOfCoins);
-
+        this.CollectedCoins = this.CollectedCoins + 1;
+        Debug.Log("Collected " + this.CollectedCoins + " / " + this.TotalNumberOfCoins + " coins.");
+        CoinCounter.text = "Coins: " + this.CollectedCoins + " / " + this.TotalNumberOfCoins;
     }
 }
